f69d04adb12bfcc73d775eb6a82d024e4cffbc48
sho ochiai <ochi1020@gmail.com>
2021-01-20 19:45:59 +0900

    feat: first commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Counter from './components/Counter';
+import { createStore } from 'redux';
 
-function App() {
+function reducer(state, action) {
+  console.log('reducer', state, action);
+  return state;
+}
+
+const store = createStore(reducer);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter/>
     </div>
   );
 }
