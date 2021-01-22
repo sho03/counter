677a0b2d1a8a073fd4b51b0cc9aaced3b918e38a
Sho Ochiai <ochiai_s@welmo.co.jp>
2021-01-22 19:23:28 +0900

    feat: commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import Counter from './components/Counter';
 import { createStore } from 'redux';
+import React from 'react';
 
-function reducer(state, action) {
+const initialState = {
+  count: 0
+}
+
+function reducer(state = initialState, action) {
   console.log('reducer', state, action);
-  return state;
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      }
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      }
+    case 'RESET':
+      return {
+        count : 0
+      }
+    default:
+      return state
+  }
 }
 
 const store = createStore(reducer);
+store.dispatch({type: "INCREMENT"})
+store.dispatch({type: "INCREMENT"})
+store.dispatch({type: "DECREMENT"})
+store.dispatch({type: "RESET"})
 
 const App = () => {
   return (
